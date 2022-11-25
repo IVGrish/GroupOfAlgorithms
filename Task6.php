d3ab88c92e4ffe2208d100d35d82d7147937a230
IVGrish <55291439+IVGrish@users.noreply.github.com>
2022-11-25 17:39:31 +0300

    Delete Task6.php

--- Task6.php
+++
@@ -1,9 +0,0 @@
-<?php
-function stringCapTrim(string $input): string
-{
-    return str_replace(array(' ', '-', '_'), '',
-           ucwords(strtolower(trim($input)), ' -_'));
-}
-
-$string = '      The quick-brown_fox jumps over the_lazy-dog     ';
-print_r(stringCapTrim($string));
