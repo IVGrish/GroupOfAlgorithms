9f6f119da326b78a2343ab761653cbf9e5a9485a
Ivan <ivan06042000@gmail.com>
2022-11-26 06:02:49 +0300

    bugs fixes

--- Task2.php
+++ Task2.php
@@ -9,10 +9,30 @@ function birthdayCountdown(string $date): string
         echo 'Wrong date and time: ' . $e->getMessage();
         exit();
     }
-    $years = $birthdate->diff($now)->y + 1;
-    $birthdate->modify("+$years years");
+    if ($now->format("Y") > $birthdate->format("Y"))
+    {
+        $years = $birthdate->diff($now)->y + 1;
+        $birthdate->modify("+$years years +1 day");
+    }
+    elseif ($now->format("m")  >  $birthdate->format("m") &&
+              $now->format("Y") === $birthdate->format("Y"))
+    {
+        $years = $birthdate->diff($now)->y + 1;
+        $birthdate->modify("+$years years +1 day");
+    }
+    elseif ($now->format("d")  >  $birthdate->format("d") &&
+              $now->format("m") === $birthdate->format("m") &&
+              $now->format("Y") === $birthdate->format("Y"))
+    {
+        $years = $birthdate->diff($now)->y + 1;
+        $birthdate->modify("+$years years +1 day");
+    }
+    else
+    {
+        $birthdate->modify("+1 days");
+    }
     $interval = $now->diff($birthdate);
     return $interval->format('%R%a days');
 }
 
-echo birthdayCountdown('06.04.2000');
+echo birthdayCountdown('26.11.2022');
